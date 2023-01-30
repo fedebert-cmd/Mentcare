9b95492eaef207dc2bd9871018ac0978117e41e1
fedebert-cmd <bertellifederico95@gmail.com>
2023-01-30 17:01:41 +0100

    primo commit

---
+++ app/src/main/java/it/univr/MentcareApp.java
@@ -0,0 +1,9 @@
+package it.univr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MentcareApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MentcareApp.class, args);
+    }
+}
